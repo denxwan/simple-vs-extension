938d15a5d564b9203cf5fbfec6eb1a0c44566d7a
Haritha Denuwan <harithadenuwan01@gmail.com>
2023-01-17 02:25:16 +0530

    Added images for every level. In working state.

--- extension.js
+++ extension.js
@@ -10,12 +10,107 @@ const vscode = require('vscode');
  */
 function activate(context) {
 
-	let tuples = vscode.languages.getDiagnostics();
+	//var imageLink;
     //let basePath = vscode.workspace.rootPath;
 	//let baseLength = basePath.length + 1;
 	//console.log(tuples);
+	
+	// // variables
+	// let tuples = vscode.languages.getDiagnostics();
+	// var totalErrorCount = 0;
 
+	// // getting the error count
+	// for (var [thisUri,thisDiagnostics] of tuples) {
+	// 	for (let thisDiagnostic of thisDiagnostics) {
+	// 		totalErrorCount++;
+	// 	}
+	// }
+
+	// // calculating the error level
+	// if (totalErrorCount > 0 && totalErrorCount <= 2) {
+	// 	// low level
+	// 	console.log("\u001b[1;31mError status: low level");
+	// }
+	// else if (totalErrorCount > 2 && totalErrorCount <= 5) {
+	// 	// moderate level
+	// 	console.log("\u001b[1;31mError status: moderate level");
+	// }
+	// else if (totalErrorCount > 5 && totalErrorCount <= 10) {
+	// 	// severe level
+	// 	console.log("\u001b[1;31mError status: severe level");
+	// }
+	// else if (totalErrorCount > 10) {
+	// 	// oh boi you got it level
+	// 	console.log("\u001b[1;31mError status: critical level");
+	// 	let imageLink='https://s3.getstickerpack.com/storage/uploads/sticker-pack/mr-incredible-pack/sticker_9.webp';
+	// }
+
+
+	// debugging
+	//console.log("Total number of errors : " + totalErrorCount);
+
+	// listning to updates on code
+	let currentEditor = vscode.window.activeTextEditor;
+	let listner = vscode.workspace.onDidChangeTextDocument((currentEditor) => {
+			// console.log('something changed!');
+			checkErrors();
+		}
+	)
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with  registerCommand
+	// The commandId parameter must match the command field in package.json
+
+	// track currently webview panel
+	let currentPanel = vscode.WebviewPanel | undefined;
+
+	let disposable = vscode.commands.registerCommand('simple-vs-extension.helloWorld', function () {
+		// The code you place here will be executed every time your command is executed
+
+		// Display a message box to the user
+		//vscode.window.showInformationMessage('Hello World from Simple VS Extension!');
+
+
+		// simple webview
+
+		const columnToShowIn = vscode.window.activeTextEditor ? vscode.window.activeTextEditor.viewColumn : undefined;
+
+		if (currentPanel) {
+			currentPanel.reveal(columnToShowIn);
+		} else {
+			const panel = vscode.window.createWebviewPanel(
+				'catCoding',
+				'Cat Coding',
+				vscode.ViewColumn.Nine,
+				//columnToShowIn.Nine,
+				{}
+			);
+	
+			// HTML content
+			panel.webview.html = getWebviewContent(checkErrors());
+
+			const updateWebView = () => {
+				panel.webview.html = getWebviewContent(checkErrors());
+			}
+
+			setInterval(updateWebView, 500);
+		}
+
+
+
+		// --> console.log(vscode.languages.getDiagnostics());
+	});
+
+	context.subscriptions.push(disposable);
+
+}
+
+function checkErrors() {
+	// variables
+	let tuples = vscode.languages.getDiagnostics();
 	var totalErrorCount = 0;
+	let imageLink;
+	var objArr = [];
 
 	// getting the error count
 	for (var [thisUri,thisDiagnostics] of tuples) {
@@ -27,40 +122,41 @@ function activate(context) {
 	// calculating the error level
 	if (totalErrorCount > 0 && totalErrorCount <= 2) {
 		// low level
-		console.log("\u001b[1;31mError status: low level");
+		//console.log("\u001b[1;31mError status: low level");
+		imageLink='https://s3.getstickerpack.com/storage/uploads/sticker-pack/mr-incredible-pack/sticker_2.webp';
 	}
 	else if (totalErrorCount > 2 && totalErrorCount <= 5) {
 		// moderate level
-		console.log("\u001b[1;31mError status: moderate level");
+		//console.log("\u001b[1;31mError status: moderate level");
+		imageLink='https://s3.getstickerpack.com/storage/uploads/sticker-pack/mr-incredible-pack/sticker_8.webp';
 	}
 	else if (totalErrorCount > 5 && totalErrorCount <= 10) {
 		// severe level
-		console.log("\u001b[1;31mError status: severe level");
+		//console.log("\u001b[1;31mError status: severe level");
+		imageLink='https://s3.getstickerpack.com/storage/uploads/sticker-pack/mr-incredible-pack/sticker_9.webp';
 	}
 	else if (totalErrorCount > 10) {
 		// oh boi you got it level
-		console.log("\u001b[1;31mError status: critical level");
-		
+		//console.log("\u001b[1;31mError status: critical level");
+		imageLink='https://s3.getstickerpack.com/storage/uploads/sticker-pack/mr-incredible-pack/sticker_14.webp';
 	}
 
+	objArr.push(imageLink);
+	objArr.push(totalErrorCount);
 
-	// debugging
-	console.log("Total number of errors : " + totalErrorCount);
+	return objArr;
+}
 
-
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('simple-vs-extension.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		//vscode.window.showInformationMessage('Hello World from Simple VS Extension!');
-		// --> console.log(vscode.languages.getDiagnostics());
-	});
-
-	context.subscriptions.push(disposable);
+function getWebviewContent(objArr) {
+	return `
+		<html>
+			<body>
+				<hr>
+				<img src=${objArr[0]} width=200px/>
+				<h4>⚠️ Active errors count: ${objArr[1]}</h4>
+			</body>
+		</html>
+	`;
 }
 
 // This method is called when your extension is deactivated
@@ -70,26 +166,3 @@ module.exports = {
 	activate,
 	deactivate
 }
-
-
-// let tuples = languages.getDiagnostics();
-//     let basePath = workspace.rootPath;
-//     let baseLength = basePath.length + 1;
-//     let diagnosticOutputs: diagnosticOutput[] = [];
-//     for (var [thisUri,thisDiagnostics] of tuples) {
-//         for (let thisDiagnostic of thisDiagnostics) {
-//             let myDiagnosticOutput : diagnosticOutput = new diagnosticOutput();
-//             if (thisUri.fsPath.startsWith(basePath)) {
-//                 myDiagnosticOutput.fsPath = thisUri.fsPath.substring(baseLength);
-//             } else {
-//                 myDiagnosticOutput.fsPath = thisUri.fsPath;
-//             }
-//             myDiagnosticOutput.code = thisDiagnostic.code;
-//             myDiagnosticOutput.message = thisDiagnostic.message;
-//             myDiagnosticOutput.startLine = thisDiagnostic.range.start.line;
-//             myDiagnosticOutput.startCharacter = thisDiagnostic.range.start.character;
-//             myDiagnosticOutput.endLine = thisDiagnostic.range.end.line;
-//             myDiagnosticOutput.endCharacter = thisDiagnostic.range.end.character;
-//             diagnosticOutputs.push(myDiagnosticOutput);
-//         }
-//     }
